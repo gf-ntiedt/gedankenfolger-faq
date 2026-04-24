3c1525b0bb9d7a311972f9b4cbb660f4f89a8b9b
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-04-24 14:27:50 +0200

    chore: migrate from v13 to v14 (fix deprecations)

--- ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('TYPO3') || die();
-
-// Allow our data type table on standard pages when using classic TCA fallback if needed.
-// Kept minimal because records are defined via Content Blocks DataType.
