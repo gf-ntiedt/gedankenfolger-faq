c178b50f00be86a33b9d1e8368d194e820872d49
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-05-05 15:56:12 +0200

    feat: add classic static template for installations without SiteSets
    Registers Configuration/TypoScript/ as a static template via ext_tables.php,
    allowing TypoScript inclusion through the template record on TYPO3 installs
    that do not use the Site Set mechanism.

---
+++ ext_tables.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('TYPO3') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'gedankenfolger_faq',
+    'Configuration/TypoScript/',
+    'Gedankenfolger FAQ'
+);
