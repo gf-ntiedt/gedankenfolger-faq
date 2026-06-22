41edae68d1c2101a2b9b073d3a35c708c0bed215
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-06-22 16:01:36 +0200

    chore(release): bump version to 14.2.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'niels.tiedt@gedankenfolger.de',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '14.2.4',
+    'version' => '14.2.5',
     'autoload' => [
         'psr-4' => [
             'Gedankenfolger\\GedankenfolgerFaq\\' => 'Classes',
@@ -16,13 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.99.99',
+            'typo3' => '14.3.0-14.99.99',
             'content_blocks' => '2.0.0-2.99.99',
+            'ws_scss' => '14.0.0-14.99.99',
+            'schema' => '4.0.0-4.99.99',
         ],
         'conflicts' => [],
-        'suggests' => [
-            'ws_scss' => '',
-            'schema' => '',
-        ],
+        'suggests' => [],
     ],
 ];
